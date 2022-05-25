fe7647bf581a509b5d88991f21c5b5940fc4f587
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-05-25 10:10:42 +0700

    Create from.jsx

---
+++ js/from.jsx
@@ -0,0 +1,28 @@
+<script src ="https://form.jotform.com/static/feedback2.js" type="text/javascript"></script><script type="text/javascript">
+        
+        var JFL_211958944889075 = new JotformFeedback({
+          formId: '211958944889075',
+          base: 'https://form.jotform.com/',
+          windowTitle: 'Formulir',
+          background: '#f5a623',
+          fontColor: '#FFFFFF',
+          type: '1',
+          height: 900,
+          width: 400,
+           openOnLoad:true
+          
+        });
+    </script>
+    <script language="JavaScript">
+    
+    var delayseconds = 5;
+    
+    window.onload = closeLightbox();
+    
+    function closeLightbox() {
+    
+    setTimeout(function(){document.querySelector('.jt-dimmer').click();}, delayseconds*5000);
+    
+    }
+    
+    </script>
